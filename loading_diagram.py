ab90bfcfdc6bd3bd7901a6575427e6d8a25c6455
LuukDevine <l.t.dewijn@student.tudelft.nl>
2024-04-02 12:31:17 +0200

    Update m_wing value in main.py

--- loading_diagram.py
+++ loading_diagram.py
@@ -18,13 +18,12 @@ b_n = 1.70 #nacelle width [m]
 l_n = -9.86 #nacelle arm length [m] FIND THIS VALUE!!!!
 l_h = 16 #horizontal tail arm [m]
 l_fn = 14.1 #distance from the wing leading to nose [m]?? not sure about this
-cg = 0.5 #center of gravity location
 Lambda_w = 17.5 *np.pi/180 #sweep angle [rad]
 Lambda_h_4 = 26 *np.pi/180#sweep angle [rad]
 Lambda_h_2 = 19.61 *np.pi/180 #sweep angle [rad]
 horizontal_efficiency = 0.8 #horizontal tail efficiency
 oswald_efficiency = 0.8 #horizontal tail efficiency
-downwash_rate = 0.75 #downwash rate
+downwash_rate = 0 #downwash per AOA, 0 for T-tail
 V_ratio = 1 #vertical tail velocity ratio
 V_cruise = 234.6 #cruise speed [m/s] 
 V_approach = 65.8333 #approach speed [m/s] 
@@ -55,6 +54,13 @@ S_net = S-S_overlap #net wing aread
 ac_w_cruise = 0.40 #wing ac location at cruise (FROM GRAPH ADSEE SLIDES L7 S36)
 ac_w_approach = 0.28 #wing ac location at approach
 
+print("mach cruise:", M_cruise)
+print("beta*A cruise:", beta_cruise*A_w)
+print("beta*A approach:", beta_approach*A_w)
+print("tan sweep/beta cruise:", np.tan(Lambda_w)/beta_cruise)
+print("tan sweep/beta cruise:", np.tan(Lambda_w)/beta_approach)
+print("taper wing:", lambda_w)
+
 # ======== LOADING DIAGRAM SHIT ========
 
 #CG VALUES
@@ -71,21 +77,21 @@ passenger_weight = 90 #average passenger weight in [kg] incl. carry-on luggage
 total_cargo_volume = 24.87 #total cargo volume in m^3
 cargo_density = 150 #cargo density in kg/m^3
 
-cargo_front_fraction = 0.5 #fraction of cargo in front cargo hold FIND THESE 
-cargo_rear_fraction = 0.5 #fraction of cargo in front cargo hold
+cargo_front_fraction = 0.7 #fraction of cargo in front cargo hold FIND THESE 
+cargo_rear_fraction = 0.3 #fraction of cargo in front cargo hold
 
 cargo_weight_front = cargo_front_fraction * total_cargo_volume * cargo_density
 cargo_weight_rear = cargo_rear_fraction * total_cargo_volume * cargo_density
 
-cargo_front_cg = 10 #cargo cg location of front cargo hold FIND THESE
-cargo_rear_cg = 25 #cargo cg location of rear cargo hold FIND THESE
+cargo_front_cg = 0.3*l_f #cargo cg location of front cargo hold FIND THESE
+cargo_rear_cg = 0.75*l_f #cargo cg location of rear cargo hold FIND THESE
 
 fuel_mass = 97270/9.81 #Fuel mass [kg]
 fuel_fraction_wing = 0.75 # FIND THESE FRACTIONS
 fuel_fraction_center = 0.25
 
-fuel_cg_wing = 0.6*l_f #fuel cg location in wing FIND THESE
-fuel_cg_center = 0.55*l_f #fuel cg location in center
+fuel_cg_wing = 0.55*l_f #fuel cg location in wing FIND THESE
+fuel_cg_center = 0.5*l_f #fuel cg location in center
 
 full_cg_list = np.array([])
 
@@ -103,7 +109,6 @@ list_cg_cargo_rear = np.array([OEW_cg ,calculate_new_cg(OEW_kg,OEW_cg,cargo_weig
 list_weight_cargo_front = np.array([OEW_kg,OEW_kg + cargo_weight_front, OEW_kg + cargo_weight_front + cargo_weight_rear])
 list_weight_cargo_rear = np.array([OEW_kg,OEW_kg + cargo_weight_rear, OEW_kg + cargo_weight_front + cargo_weight_rear])
 
-print(list_cg_cargo_front)
 current_weight = OEW_kg + cargo_weight_front + cargo_weight_rear
 current_cg = list_cg_cargo_front[-1]
 
@@ -124,7 +129,6 @@ aisle_weight = np.array([])
 window_weight = np.append(window_weight, current_weight)
 list_cg_window_front = np.append(list_cg_window_front, current_cg)
 list_cg_window_back = np.append(list_cg_window_back, current_cg)
-print(window_weight)
 
 #WINDOW SEAT LOADING
 for i in range(seat_rows+1):
@@ -159,9 +163,7 @@ full_cg_list = np.append(full_cg_list, list_cg_aisle_back)
 middle_weight = np.append(middle_weight, current_weight)
 list_cg_middle_front = np.append(list_cg_middle_front, list_cg_aisle_back[-1])
 list_cg_middle_back = np.append(list_cg_middle_back, list_cg_aisle_back[-1])
-print(middle_weight)
-print(list_cg_middle_front)
-print(list_cg_middle_back)
+
 
 for i in range(seat_rows):
     cg_middle_front = calculate_new_cg(current_weight, list_cg_middle_front[i], passenger_weight, front_seat_cg + i*seat_pitch) # FRONT TO BACK 
@@ -196,53 +198,70 @@ full_cg_list = np.append(full_cg_list, list_cg_fuel_wing_center)
 cg_min = np.min(full_cg_list)
 cg_max = np.max(full_cg_list)
 
+print("cg_min [m]:", cg_min)
+print("cg_max [m]:", cg_max)
 
+print("OEW_cg:", OEW_cg)
+print("OEW_cg MAC:", calculate_MAC_percentage(OEW_cg))
+
+print("front cargo cg:", cargo_front_cg)
+print("front cargo cg MAC:", calculate_MAC_percentage(cargo_front_cg))
+
+print("rear cargo cg:", cargo_rear_cg)
+print("rear cargo cg MAC:", calculate_MAC_percentage(cargo_rear_cg))
+
+print("fuel cg wing:", fuel_cg_wing)
+print("fuel cg wing MAC:", calculate_MAC_percentage(fuel_cg_wing))
+print("fuel cg center:", fuel_cg_center)
+print("fuel cg center MAC:", calculate_MAC_percentage(fuel_cg_center))
 
 #PLOTTING OF LOADING DIAGRAM
-plt.plot(list_cg_cargo_front, list_weight_cargo_front, label='Loading front to back')
-plt.plot(list_cg_cargo_rear, list_weight_cargo_rear, label='Loading back to front')
-plt.plot(list_cg_window_front, window_weight, label='Window seat front to back')
-plt.plot(list_cg_window_back, window_weight, label='Window seat back to front')
-plt.plot(list_cg_aisle_front, aisle_weight, label='Aisle seat front to back')
-plt.plot(list_cg_aisle_back, aisle_weight, label='Aisle seat back to front')
-plt.plot(list_cg_middle_front, middle_weight, label='middle seat front to back')
-plt.plot(list_cg_middle_back, middle_weight, label='middle seat back to front')
-plt.plot(list_cg_fuel_wing_center, fuel_weight, label='middle seat back to front')
-plt.axvline(x=cg_min, color='r', linestyle='--', label='CG Min')
-plt.axvline(x=cg_max, color='b', linestyle='--', label='CG Max')
-plt.plot
-plt.xlabel('Center of gravithy location [m]')
-plt.ylabel('weight [m]')
-plt.title('Loading Diagram')
-plt.show()
+import matplotlib.pyplot as plt
 
-plt.plot(calculate_MAC_percentage(list_cg_cargo_front), list_weight_cargo_front, label='Loading front to back')
-plt.plot(calculate_MAC_percentage(list_cg_cargo_rear), list_weight_cargo_rear, label='Loading back to front')
-plt.plot(calculate_MAC_percentage(list_cg_window_front), window_weight, label='Window seat front to back')
-plt.plot(calculate_MAC_percentage(list_cg_window_back), window_weight, label='Window seat back to front')
-plt.plot(calculate_MAC_percentage(list_cg_aisle_front), aisle_weight, label='Aisle seat front to back')
-plt.plot(calculate_MAC_percentage(list_cg_aisle_back), aisle_weight, label='Aisle seat back to front')
-plt.plot(calculate_MAC_percentage(list_cg_middle_front), middle_weight, label='middle seat front to back')
-plt.plot(calculate_MAC_percentage(list_cg_middle_back), middle_weight, label='middle seat back to front')
-plt.plot(calculate_MAC_percentage(list_cg_fuel_wing_center), fuel_weight, label='middle seat back to front')
-plt.axvline(x=calculate_MAC_percentage(cg_min), color='r', linestyle='--', label='CG Min')
-plt.axvline(x=calculate_MAC_percentage(cg_max), color='b', linestyle='--', label='CG Max')
-plt.plot
-plt.xlabel('Center of gravithy location [m]')
-plt.ylabel('weight [m]')
-plt.title('Loading Diagram')
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+# First plot
+axs[0].plot(list_cg_cargo_front, list_weight_cargo_front, label='Cargo loading front to back')
+axs[0].plot(list_cg_cargo_rear, list_weight_cargo_rear, label='Cargo loading back to front')
+axs[0].plot(list_cg_window_front, window_weight, label='Window seat front to back')
+axs[0].plot(list_cg_window_back, window_weight, label='Window seat back to front')
+axs[0].plot(list_cg_aisle_front, aisle_weight, label='Aisle seat front to back')
+axs[0].plot(list_cg_aisle_back, aisle_weight, label='Aisle seat back to front')
+axs[0].plot(list_cg_middle_front, middle_weight, label='middle seat front to back')
+axs[0].plot(list_cg_middle_back, middle_weight, label='middle seat back to front')
+axs[0].plot(list_cg_fuel_wing_center, fuel_weight, label='Fuel')
+axs[0].axvline(x=cg_min, color='r', linestyle='--', label='CG Min')
+axs[0].axvline(x=cg_max, color='b', linestyle='--', label='CG Max')
+axs[0].set_xlabel('Center of gravity location [m]')
+axs[0].set_ylabel('Weight [kg]')
+axs[0].set_title('Loading Diagram')
+axs[0].legend()
+
+# Second plot
+axs[1].plot(calculate_MAC_percentage(list_cg_cargo_front), list_weight_cargo_front, label='Cargo loading front to back')
+axs[1].plot(calculate_MAC_percentage(list_cg_cargo_rear), list_weight_cargo_rear, label='Cargo loading back to front')
+axs[1].plot(calculate_MAC_percentage(list_cg_window_front), window_weight, label='Window seat front to back')
+axs[1].plot(calculate_MAC_percentage(list_cg_window_back), window_weight, label='Window seat back to front')
+axs[1].plot(calculate_MAC_percentage(list_cg_aisle_front), aisle_weight, label='Aisle seat front to back')
+axs[1].plot(calculate_MAC_percentage(list_cg_aisle_back), aisle_weight, label='Aisle seat back to front')
+axs[1].plot(calculate_MAC_percentage(list_cg_middle_front), middle_weight, label='middle seat front to back')
+axs[1].plot(calculate_MAC_percentage(list_cg_middle_back), middle_weight, label='middle seat back to front')
+axs[1].plot(calculate_MAC_percentage(list_cg_fuel_wing_center), fuel_weight, label='Fuel')
+axs[1].axvline(x=calculate_MAC_percentage(cg_min), color='r', linestyle='--', label='CG Min')
+axs[1].axvline(x=calculate_MAC_percentage(cg_max), color='b', linestyle='--', label='CG Max')
+axs[1].set_xlabel('Center of gravity location [MAC]')
+axs[1].set_ylabel('Weight [kg]')
+axs[1].set_title('Loading Diagram MAC percentage')
+axs[1].legend()
+
+plt.tight_layout()
 plt.show()
 
 cg_min = calculate_MAC_percentage(cg_min) 
 cg_max = calculate_MAC_percentage(cg_max)
-
-print("mach cruise:", M_cruise)
-print("beta*A cruise:", beta_cruise*A_w)
-print("beta*A approach:", beta_approach*A_w)
-print("tan sweep/beta cruise:", np.tan(Lambda_w)/beta_cruise)
-print("tan sweep/beta cruise:", np.tan(Lambda_w)/beta_approach)
-print("taper wing:", lambda_w)
-
+cg = (cg_min+cg_max)/2
+print("cg_min:", cg_min)
+print("cg_max:", cg_max)
 
 # ======== STABILITY FUNCTIONS ========
 def calculate_C_L_alpha(A, beta, n, Lambda): #for clalpha input either wing data or tail data to get the respective clalpha
@@ -261,8 +280,8 @@ def calculate_wf_x_ac_chord(C_L_alpha_A_h, sweep_angle, taper, x_ac_w, cg):
     x_ac_wf = x_ac_w - 1.8/C_L_alpha_A_h * (b_f*h_f*l_fn)/(S*c) + 0.273/(1+taper) * (b_f*cg*(b-b_f))/(c**2 * (b+2.15*b_f)) * np.tan(sweep_angle)
     return x_ac_wf #return X_ac/c contribution due to wing and fuselage
 
-def stability_curve(C_L_h, C_L_alpha_A_h, downwash, l_h, c, tail_velocity_ratio, x_cg, x_ac): #S/S_h for stability, witch x = Cg/MAC
-    Sh_S = 1/((C_L_h/C_L_alpha_A_h) * (1-downwash)* l_h/c * tail_velocity_ratio**2 ) * x_cg - (x_ac-0.05)/((C_L_h/C_L_alpha_A_h) * (1-downwash)* l_h/c * tail_velocity_ratio**2 )
+def stability_curve(C_L_alpha_h, C_L_alpha_A_h, downwash, l_h, c, tail_velocity_ratio, x_cg, x_ac, safety_factor): #S/S_h for stability, witch x = Cg/MAC
+    Sh_S = 1/((C_L_alpha_h/C_L_alpha_A_h) * (1-downwash)* l_h/c * tail_velocity_ratio**2 ) * x_cg - (x_ac-safety_factor)/((C_L_alpha_h/C_L_alpha_A_h) * (1-downwash)* l_h/c * tail_velocity_ratio**2 )
     return Sh_S
 
 # ======== STABILITY CALCULATIONS ========
@@ -283,7 +302,7 @@ x_ac_wf_approach = calculate_wf_x_ac_chord(C_L_alpha_A_h_cruise, Lambda_w, lambd
 x_ac_cruise = x_ac_nacelle_cruise + x_ac_wf_cruise #Total shift of ac is the shift due to nacelle + shift due to wing/fuselage
 x_ac_approach = x_ac_nacelle_approach + x_ac_wf_approach
 
-Sh_S = stability_curve(C_L_alpha_h_cruise, C_L_alpha_A_h_cruise, downwash_rate, l_h, c, V_ratio, x_ac_cruise, x_ac_cruise)
+Sh_S = stability_curve(C_L_alpha_h_cruise, C_L_alpha_A_h_cruise, downwash_rate, l_h, c, V_ratio, x_ac_cruise, x_ac_cruise,0.05)
 
 # ======== CONTROLLABILITY FUNCTIONS ========
 
@@ -309,10 +328,8 @@ def calculate_C_L_A_h(rho, V, S, S_h, Weight): #IDK how to find this value so i 
 def contrallability_curve(C_L_h, C_L_A_h, lh, c, V_ratio, x_cg, x_ac, C_m_ac):
     Sh_S_contrallability = 1/(C_L_h/C_L_A_h * lh/c * V_ratio**2) * x_cg + (C_m_ac/C_L_A_h - x_ac)/(C_L_h/C_L_A_h * lh/c * V_ratio**2)
     return Sh_S_contrallability
-
+ 
 # ======== CONTROLLABILITY CALCULATIONS ========
-
-C_L_alpha_h_cruise = C_L_alpha_A_h_cruise
 
 C_L_A_h_cruise = calculate_C_L_A_h(rho_cruise, V_cruise, S, S_h, MTOW)
 C_L_A_h_approach = calculate_C_L_A_h(rho_0, V_approach, S, S_h, MTOW)
@@ -352,19 +369,21 @@ print("C_m_ac_cruise", C_m_ac_cruise)
 
 # ======== PLOTTING OF RESULTS ========
 
-x_cg_values = np.linspace(0, 1, 100) #Creates a range of cg values to plot the stability and controllability curves
-S_Sh_values_stability_cruise = stability_curve(C_L_alpha_h_cruise, C_L_alpha_A_h_cruise, downwash_rate, l_h, c, V_ratio, x_cg_values, x_ac_cruise)
+x_cg_values = np.linspace(-0, 1.3, 100) #Creates a range of cg values to plot the stability and controllability curves
+S_Sh_values_stability_cruise = stability_curve(C_L_alpha_h_cruise, C_L_alpha_A_h_cruise, downwash_rate, l_h, c, V_ratio, x_cg_values, x_ac_cruise, 0.05)
+S_Sh_values_neutral_stability_cruise = stability_curve(C_L_alpha_h_cruise, C_L_alpha_A_h_cruise, downwash_rate, l_h, c, V_ratio, x_cg_values, x_ac_cruise, 0)
 S_Sh_contrallability_cruise = contrallability_curve(C_L_h, C_L_A_h_cruise, l_h, c, V_ratio, x_cg_values, x_ac_cruise, C_m_ac_cruise)
 
 plt.plot(x_cg_values, S_Sh_contrallability_cruise, label='Controllability Cruise')
 plt.plot(x_cg_values, S_Sh_values_stability_cruise, label='Stability Cruise', color='green')
+plt.plot(x_cg_values, S_Sh_values_neutral_stability_cruise, label='Neutral Stability Cruise', color='yellow')
 plt.fill_between(x_cg_values, S_Sh_values_stability_cruise, 0, where=(S_Sh_values_stability_cruise > 0), color='red', alpha=0.2) #Stability curve for cruise
 plt.fill_between(x_cg_values, S_Sh_contrallability_cruise, 0, where=(S_Sh_contrallability_cruise > 0), color='red', alpha=0.2) #Stability curve for controllability
-plt.axhline(y=S_h/S, color='grey', linestyle='--')
+plt.axhline(y=S_h/S, color='grey', linestyle='--', label ='S_h/S for F100')
 plt.plot([cg_min, cg_max], [S_h/S, S_h/S], color='red', label='Operative Cg range')
 plt.xlabel('Center of Gravity Location [MAC]')
 plt.ylabel('S_h/S')
-plt.title('Stability Curve')
+plt.title('Stability and controllability Curve')
 plt.grid(True)
 plt.ylim(bottom=0)  # Limit y-axis to y > 0
 plt.legend()
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 MTOW = 449240
 
 m_prop = 0.14 * MTOW
-m_wing = 0.1 * MTOW
+m_wing = 0.12 * MTOW
+
+
